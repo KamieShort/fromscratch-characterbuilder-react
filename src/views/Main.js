796e380e87d9059c1abe0d386fd7929d4f33648d
Kamie Short <kamie.short@gmail.com>
2022-03-09 16:13:29 -0800

    set useState and pass state variables as props

---
+++ src/views/Main.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+import Controls from '../components/Controls';
+import Head from '../components/Head/Head';
+import Middle from '../components/Middle/Middle';
+import Bottom from '../components/Bottom/Bottom';
+import Catchprase from '../components/Catchphrase/Catchphrase';
+
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+export default function Main() {
+  const [head, setHead] = useState('head');
+  const [middle, setMiddle] = useState('middle');
+  const [bottom, setBottom] = useState('bottom');
+  const [catchphrase, setCatchprase] = useState('');
+
+  return (
+    <div>
+      <Controls setHead={setHead} setMiddle={setMiddle} setBottom={setBottom} />
+      <Head head={head} />
+      <Middle middle={middle} />
+      <Bottom bottom={bottom} />
+      <Catchprase catchphrase={catchphrase} setCatchprase={setCatchprase} />
+    </div>
+  );
+}
