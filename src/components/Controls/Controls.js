d567ac55a0f662516c4b0d19fd79622e6c760d51
Kamie Short <kamie.short@gmail.com>
2022-03-09 18:06:01 -0800

    added labels to dropdowns

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -2,41 +2,50 @@ import React from 'react';
 
 export default function Controls({ setHead, setMiddle, setBottom }) {
   return (
-    <div>
-      <select
-        onChange={(e) => {
-          setHead(e.target.value);
-        }}
-      >
-        <option value="bird">Bird</option>
-        <option value="duck">Duck</option>
-        <option value="dog">Dog</option>
-        <option value="horse">Horse</option>
-      </select>
-
-      <select
-        onChange={(e) => {
-          setMiddle(e.target.value);
-        }}
-      >
-        <option value="blue">Blue</option>
-        <option value="dress">Fancy</option>
-        <option value="pink">Pink</option>
-        <option value="red">Red</option>
-      </select>
-
-      <select
-        onChange={(e) => {
-          setBottom(e.target.value);
-        }}
-      >
-        <option value="leg">Single Leg</option>
-        <option value="white">White Pants</option>
-        <option value="blue">Blue Jeans</option>
-      </select>
-
-      <input id="catchphrase-input" />
-      <button id="catchphrase-button">Add</button>
+    <div className="controls">
+      <label>
+        Choose a head
+        <select
+          onChange={(e) => {
+            setHead(e.target.value);
+          }}
+        >
+          <option value="bird">Bird</option>
+          <option value="duck">Duck</option>
+          <option value="dog">Dog</option>
+          <option value="horse">Horse</option>
+        </select>
+      </label>
+      <label>
+        Choose a Middle:
+        <select
+          onChange={(e) => {
+            setMiddle(e.target.value);
+          }}
+        >
+          <option value="blue">Blue</option>
+          <option value="dress">Fancy</option>
+          <option value="pink">Pink</option>
+          <option value="red">Red</option>
+        </select>
+      </label>
+      <label>
+        Choose a Bottom:
+        <select
+          onChange={(e) => {
+            setBottom(e.target.value);
+          }}
+        >
+          <option value="leg">Single Leg</option>
+          <option value="white">White Pants</option>
+          <option value="blue">Blue Jeans</option>
+        </select>
+      </label>
+      <label>
+        Write your own catchprase
+        <input id="catchphrase-input" />
+        <button id="catchphrase-button">Add</button>
+      </label>
     </div>
   );
 }
